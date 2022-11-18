c25f58b3af4363904d13b8d7fd7e65413abbdfe2
OneZerocococo <ylflyfly@gmail.com>
2022-11-18 15:54:39 +0800

    add message partials

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = 3000
 const session = require('express-session')
 const usePassport = require('./config/passport')
 const routes = require('./routes')
+const flash = require('connect-flash')
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(session({
@@ -16,9 +17,12 @@ app.use(session({
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
