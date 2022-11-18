ccbc5f712b80a4981dc8d836de37a75e730e7ca4
OneZerocococo <ylflyfly@gmail.com>
2022-11-18 16:11:59 +0800

    add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,10 +3,55 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+  console.log(req.user.id)
+  return res.render('new')
+})
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  return Todo.findOne({ _id, userId })
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.findOne({ _id, userId })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${_id}`))
+    .catch(error => console.log(error))
+})
+
+router.delete('/:id', (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  return Todo.findOne({ _id, userId })
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
